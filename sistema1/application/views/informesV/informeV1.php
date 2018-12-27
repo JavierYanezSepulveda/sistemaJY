466732396a7e6d808029a2a1ed14a0aa4eaa6855
JavierYanezSepulveda <javierandres.y.s@gmail.com>
2018-12-27 15:40:50 -0300

    1

---
+++ sistema1/application/views/informesV/informeV1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html >
+
+   <body>
+    <div class="container" style="margin-left: 13%">
+    <h1>Buscar</h1>
+
+    <form id="buscar" method="GET" action=" <?php echo base_url();?>index.php/Informes/informesVentas2">
+
+
+        <label>De:</label>
+        <input type="date" id="query1" name="query1" value="<?php echo date("Y-m-d");?>">
+        <label>Hasta:</label>
+        <input type="date" id="query2" name="query2" value="<?php echo date("Y-m-d");?>">
+        <input type="hidden" id="id_sucursal" name="id_sucursal" value="<?php echo $this->session->userdata('ID_SUCURSAL');?>">
+        <input type="submit"  id="buscar" value="buscar" class="btn UCM">
+ </form>
+
+ 
+
+ <br> <br>
+
+<a href="<?php echo base_url(); ?>index.php/informes" class="btn btn-danger" >Volver</a> </div>
+
+</body>       
+</html>
